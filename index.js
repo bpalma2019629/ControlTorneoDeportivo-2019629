8cbe2209441c6ec06203d3f19594da9a51d185be
bpalma <bpalma-2019629@kinal.edu.gt>
2022-06-06 09:06:34 -0600

    Hokeru

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/ControlTorneo2019629',
 {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
  console.log('Se establecio conexion con la base de datos');
- app.listen(3000, function(){
+ app.listen(process.env.PORT || 3000, function(){
      console.log('el control se esta ejecutando en el puerto 3000');
      usuariosController.UsuarioInicial();
  });   
